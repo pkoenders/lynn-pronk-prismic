b4d03ab47af0078fbce266bf6303554c38480804
Peter Koenders <pkoenders@gmail.com>
2025-10-18 16:44:48 +1300

    colour

--- src/components/slices/cards/item.js
+++ src/components/slices/cards/item.js
@@ -16,6 +16,7 @@ import IconMaterial from '/src/components/common/icons/material'
 
 import styled from 'styled-components'
 const CardsWrapper = styled.div`
+  color: inherit;
   a,
   a:link,
   a:hover,
